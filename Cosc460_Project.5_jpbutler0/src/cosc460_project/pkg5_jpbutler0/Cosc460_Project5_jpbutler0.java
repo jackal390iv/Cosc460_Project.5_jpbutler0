d1e3b914f38255c04a599558dbf498ba1fc48d59
alfred <jonpbiv@gmail.com>
2014-09-24 12:12:41 -0400

    created java project

---
+++ Cosc460_Project.5_jpbutler0/src/cosc460_project/pkg5_jpbutler0/Cosc460_Project5_jpbutler0.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cosc460_project.pkg5_jpbutler0;
+
+/**
+ *
+ * @author alfred
+ */
+public class Cosc460_Project5_jpbutler0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }    
+}
